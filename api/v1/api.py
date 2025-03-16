00408d5254f0f60921f60ffa83c72a099377fa58
Cenergy <673598118@qq.com>
2025-03-16 13:31:08 +0800

    add url

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from apps.users.routes import router as users_router
 from api.v1.auth import router as auth_router
+from apps.resources.routes import router as resources_router
 
 api_router = APIRouter()
 api_router.include_router(users_router, prefix="/users", tags=["users"])
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
+api_router.include_router(resources_router, prefix="/resources", tags=["resources"])
